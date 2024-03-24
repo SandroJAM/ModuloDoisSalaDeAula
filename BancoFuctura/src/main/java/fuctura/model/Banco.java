5b8cbfe57f6ad60fb131beecbefc3d299c4efc7e
robertocajueiro <beto.cajueiro@gmail.com>
2024-03-24 02:02:32 -0300

    First Commit

---
+++ BancoFuctura/src/main/java/fuctura/model/Banco.java
@@ -0,0 +1,46 @@
+package fuctura.model;
+
+
+public class Banco {
+
+	private int codigo;
+	private String nome;
+	private String cnpj;
+	private String Presidente;
+	
+	
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getPresidente() {
+		return Presidente;
+	}
+
+	public void setPresidente(String presidente) {
+		Presidente = presidente;
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Projeto em sala de aula fuctura
